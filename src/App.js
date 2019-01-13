baa27e2e177534a65c60ab769ee064f5004272b5
lokendra-rawat <lokendra.rawat@galaxycard.in>
2019-01-13 18:36:20 +0530

    changes to pull

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn .... React
+            Learn .... React change for aws
           </a>
         </header>
       </div>
